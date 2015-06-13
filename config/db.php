deaefd7354ae4cfa798de304e3566127edd64e47
freelancefrivas <freelance.frivas@gmail.com>
2015-06-13 07:20:39 -0300

    Revert "Yii2 Blog"
    This reverts commit 84686a7e4b1ee1460f61082ed9174634ec14308a.

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2_blog',
-    'username' => 'user',
-    'password' => 'password',
-    'charset' => 'utf8',
-];
